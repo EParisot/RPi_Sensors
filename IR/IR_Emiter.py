3435830b719f12fce506f03615f4248c1c319884
eparisot <eparisot@student.42.fr>
2019-12-11 01:06:52 +0100

    IR

---
+++ IR/IR_Emiter.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+IrPin  = 23
+count = 0
+
+def setup():
+    GPIO.setmode(GPIO.BCM)       # Numbers GPIOs by physical location
+    GPIO.setup(IrPin, GPIO.OUT)
+
+def loop():
+    while True:
+        GPIO.output(IrPin, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(IrPin, GPIO.LOW)
+        time.sleep(1)
+
+def destroy():
+    GPIO.cleanup()                     # Release resource
+
+if __name__ == '__main__':     # Program start from here
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+        destroy()
